7745806f80e218495d9cbd9b7b20d53dceab37e7
ast19004 <ast19004@byui.edu>
2022-09-08 11:54:08 -0700

    React Context

--- DummyAuthApp/src/components/Login/Login.js
+++ DummyAuthApp/src/components/Login/Login.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
+import AuthContext from '../../store/auth-context';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
@@ -38,6 +39,8 @@ const Login = (props) => {
   
   const { isValid: emailIsValid} = emailState;
   const { isValid: passwordIsValid} = passwordState;
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     const identifier = setTimeout(() => {
@@ -83,7 +86,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
