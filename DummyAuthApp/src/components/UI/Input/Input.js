653efd7220bf38f7acf2faf8a27145a7af5e2d46
ast19004 <ast19004@byui.edu>
2022-09-08 12:08:08 -0700

    Refactoring Input Component

--- DummyAuthApp/src/components/UI/Input/Input.js
+++ DummyAuthApp/src/components/UI/Input/Input.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import classes from './Input.module.css'
 
-const Input = props => {
-    return (
-        <div
-        className={`${classes.control} ${
-          emailState.isValid === false ? classes.invalid : ''
-        }`}
-      >
-        <label htmlFor="email">E-Mail</label>
-        <input
-          type="email"
-          id="email"
-          value={emailState.value}
-          onChange={emailChangeHandler}
-          onBlur={validateEmailHandler}
-        />
-      </div>
-    );
+import classes from './Input.module.css';
+
+const Input = (props) => {
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ''
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
 };
 
 export default Input;
